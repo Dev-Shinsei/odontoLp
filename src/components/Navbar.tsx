fa1a35b6aac4c0a88c044aa046c8fa4240327127
Dev-Shinsei <dev.shinsei@gmail.com>
2022-08-30 11:55:52 -0300

    navbar alterations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,23 @@
+export function Navbar() {
+  return (
+    <nav className="text-center list-none flex space-x-20 justify-between items-center py-4 px-10 bg-red-300 ">
+      <a className="cursor-pointer">
+        <img
+          src="https://github.com/Dev-Shinsei.png"
+          alt="imagem de Shinsei"
+          className=" rounded-full h-10 w-10"
+        />
+      </a>
 
-export function Navbar(){
-  return(
-    <div className="text-center bg-red-300 font-bold">
-      Navbar
-    </div>
-  )
+      <a className="cursor-pointer hover:scale-110">
+        <li>About</li>
+      </a>
+      <a className="cursor-pointer hover:scale-110">
+        <li>Resume</li>
+      </a>
+      <a className="cursor-pointer hover:scale-110">
+        <li>Contact</li>
+      </a>
+    </nav>
+  );
 }
