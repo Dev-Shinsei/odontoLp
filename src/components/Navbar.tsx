a9c192f1dac515320d9a3a4f0655ec657f567b6b
Carlos Gomes <dev.shinsei@gmail.com>
2022-08-31 20:29:55 +0000

    navbar and footer completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,32 @@
 export function Navbar() {
   return (
-    <nav className="w-full text-center list-none flex items-center justify-between bg-red-300 py-5 px-10  font-bold ">
-      <div>
-        <a
+    <nav id="home" className="relative w-full text-center list-none flex items-center justify-between bg-transparent py-5 px-10  font-bold ">
+      <a
           href="#"
           target="_blank"
-          className="cursor-pointer hover:text-white "
+          className="cursor-pointer "
         >
+          <div className="w-44 h-8  rounded flex justify-center items-center px-auto hover:bg-indigo-400">
           Jéssica Ursulino
-        </a>
-      </div>
+          </div>
+      </a>
 
-      <a className="cursor-pointer hover:text-white">
+      <a className="cursor-pointer ">
+      <div className="w-36 h-8  rounded flex justify-center items-center p-auto hover:bg-indigo-400">
         <li>About</li>
+      </div>
       </a>
 
-      <a className="cursor-pointer hover:text-white">
+      <a className="cursor-pointer ">
+      <div className="w-36 h-8  rounded flex justify-center items-center p-auto hover:bg-indigo-400">
         <li>Resume</li>
+      </div>
       </a>
 
-      <a className="cursor-pointer hover:text-white">
+      <a className="cursor-pointer">
+      <div className="w-36 h-8 rounded flex justify-center items-center p-auto hover:bg-indigo-400">
         <li>Contact</li>
+      </div>
       </a>
     </nav>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,18 +4,17 @@ import { Footer } from "../components/Footer";
 export function Home() {
   return (
     <div>
+      <img src="https://images.unsplash.com/photo-1616391182219-e080b4d1043a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1083&q=80" alt="odonto background" className="absolute object-cover bg-no-repeat w-screen h-screen opacity-20"/>
       <Navbar />
+      <div className="w-10/12 mx-auto my-0 z-index-50">
+      <main className="text-center min-h-screen">
+        <p>Home Page</p>
 
-      <main className="text-center">
-        HomePage
 
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, eaque voluptas dolorem nemo reprehenderit unde fugit repudiandae, sint autem qui quas, illo aliquid voluptatem exercitationem sunt ratione vitae possimus dicta.
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, eaque voluptas dolorem nemo reprehenderit unde fugit repudiandae, sint autem qui quas, illo aliquid voluptatem exercitationem sunt ratione vitae possimus dicta.</p>
       </main>
-
-      <footer className="relative bottom-0">
-        <Footer />
-      </footer>
+      </div>
+      <Footer />
     </div>
   );
 }
