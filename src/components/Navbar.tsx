1d676aee5bf157ca0afad661c4f24a039da325c8
Dev-Shinsei <dev.shinsei@gmail.com>
2022-08-31 11:01:12 -0300

    footer prototype completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,30 +2,26 @@ export function Navbar() {
   return (
     <nav className="w-full text-center list-none flex items-center justify-between bg-red-300 py-5 px-10  font-bold ">
       <div>
-      <a href="#" target="_blank" className="cursor-pointer hover:text-white ">
-        Jéssica Ursulino
-      </a>
+        <a
+          href="#"
+          target="_blank"
+          className="cursor-pointer hover:text-white "
+        >
+          Jéssica Ursulino
+        </a>
       </div>
-      
-      
+
       <a className="cursor-pointer hover:text-white">
         <li>About</li>
       </a>
-      
 
-      
       <a className="cursor-pointer hover:text-white">
         <li>Resume</li>
       </a>
-      
-      
+
       <a className="cursor-pointer hover:text-white">
         <li>Contact</li>
       </a>
-      
-      
-      
-      
     </nav>
   );
 }
